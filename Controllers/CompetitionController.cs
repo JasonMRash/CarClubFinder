20d8b8ac65692cfe61fccfc2b0135f9ef90b7996
JasonMRash <jasonmichaelrash@gmail.com>
2023-03-23 15:42:16 -0500

    Starting on Photo Upload

--- Controllers/CompetitionController.cs
+++ Controllers/CompetitionController.cs
@@ -28,5 +28,21 @@ namespace CarClubWebApp.Controllers
             Competition competition = await _competitionRepository.GetByIdAsync(id);
             return View(competition);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create (Competition competition)
+        {
+            if (!ModelState.IsValid)
+            { 
+                return View(competition);
+            }
+            _competitionRepository.Add(competition);
+            return RedirectToAction("Index");
+        }
     }
 }
